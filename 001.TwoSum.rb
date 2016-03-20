af14ddc7cebd0f201fb5efb0f26f53462456344a
Hang Cui <oscarcuihang@hotmail.com>
2016-03-20 18:52:45 -0500

    asd

---
+++ 001.TwoSum.rb
@@ -0,0 +1,21 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution.
+
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    map = {}
+    nums.each_with_index do |num, index|
+        i = map[target - num]
+        return [i, index] if i
+        map[num] = index
+    end
+ end
