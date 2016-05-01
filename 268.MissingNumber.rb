1300f7c74647015983862720c758fbb4ad2e0720
Hang Cui <oscarcuihang@hotmail.com>
2016-05-01 17:11:52 -0500

    268

---
+++ 268.MissingNumber.rb
@@ -0,0 +1,16 @@
+
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+# For example,
+# Given nums = [0, 1, 3] return 2.
+
+# Note:
+# Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def missing_number(nums)
+  nums.each_with_index.map{|n,i| n^(i+1)}.inject{|result,i| result^=i }
+end
