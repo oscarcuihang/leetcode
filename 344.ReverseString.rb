0803a06eb8b63c20b6146f987725dc5f769f98bc
Hang Cui <oscarcuihang@hotmail.com>
2016-05-06 20:16:29 -0500

    add

---
+++ 344.ReverseString.rb
@@ -0,0 +1,12 @@
+# Write a function that takes a string as input and returns the string reversed.
+
+# Example:
+# Given s = "hello", return "olleh".
+
+# Subscribe to see which companies asked this question
+
+# @param {String} s
+# @return {String}
+def reverse_string(s)
+    
+end
---
+++ 345.ReverseVowelsOfAString.rb
@@ -0,0 +1,15 @@
+# Write a function that takes a string as input and reverse only the vowels of a string.
+
+# Example 1:
+# Given s = "hello", return "holle".
+
+# Example 2:
+# Given s = "leetcode", return "leotcede".
+
+# Subscribe to see which companies asked this question
+
+# @param {String} s
+# @return {String}
+def reverse_vowels(s)
+    
+end
