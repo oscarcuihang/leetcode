95fb791866fbfbd6603288047fff911f0d3c9588
Hang Cui <oscarcuihang@hotmail.com>
2016-05-15 18:04:12 -0500

    343
    343

--- 343.IntegerBreak.rb
+++ 343.IntegerBreak.rb
@@ -1,4 +1,5 @@
-# Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+# Given a positive integer n, break it into the sum of at least two positive integers and 
+# maximize the product of those integers. Return the maximum product you can get.
 
 # For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
 
@@ -11,6 +12,24 @@
 
 # @param {Integer} n
 # @return {Integer}
+
 def integer_break(n)
-    
+    return n - 1 if n == 2 || n == 3
+    res = 1
+    while n > 4
+        res *= 3
+        n -= 3
+    end
+    res * n
 end
+
+# 2  = 1 + 1
+# 3  = 2 + 1
+# 4  = 2 + 4
+# 5  = 3 + 2
+# 6  = 3 + 3
+# 7  = 3 + 4
+# 8  = 3 + 3 + 2
+# 9  = 3 + 3 + 3
+# 10 = 3 + 3 + 4
+# .....
