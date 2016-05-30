40bcabf5439c709ea6c7c67cc7b16b048412bef1
Hang Cui <oscarcuihang@hotmail.com>
2016-05-30 14:20:41 -0400

    004

--- 013.RomanToInteger.rb
+++ 013.RomanToInteger.rb
@@ -1,7 +1,11 @@
+# Given a roman numeral, convert it to an integer.
 
-hash = [['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]].to_h
+# Input is guaranteed to be within the range from 1 to 3999.
 
-abc = 'MCMXCVI'	
+
+# hash = [['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]].to_h
+
+# abc = 'MCMXCVI'	
 
 s = abc.reverse!
 sum = hash[s[0]]
