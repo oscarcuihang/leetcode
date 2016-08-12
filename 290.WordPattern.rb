3939aa14984041b94f8137cb6a4043fb9098be9b
Hang Cui <oscarcuihang@hotmail.com>
2016-08-12 01:49:30 -0500

    290

---
+++ 290.WordPattern.rb
@@ -0,0 +1,29 @@
+# Given a pattern and a string str, find if str follows the same pattern.
+
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+# Examples:
+# pattern = "abba", str = "dog cat cat dog" should return true.
+# pattern = "abba", str = "dog cat cat fish" should return false.
+# pattern = "aaaa", str = "dog cat cat dog" should return false.
+# pattern = "abba", str = "dog dog dog dog" should return false.
+# Notes:
+# You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+
+
+# @param {String} pattern
+# @param {String} str
+# @return {Boolean}
+def word_pattern(pattern, str)
+    pattern = pattern.chars
+    str = str.split(' ')
+    return false if pattern.size != str.size
+    
+    p2s, s2p = {}, {}
+    pattern.zip(str) do |p, s|
+    return false if p2s.key?(p) && p2s[p] != s
+    return false if s2p.key?(s) && s2p[s] != p
+    p2s[p], s2p[s] = s, p
+    end
+    return true
+end
