2d2807136ca089bdb59fc1b8e134d52066089f06
Hang Cui <oscarcuihang@hotmail.com>
2016-09-04 17:15:03 -0500

    176

--- 176.SecondHighestSalary.sql
+++ 176.SecondHighestSalary.sql
@@ -10,7 +10,6 @@
 -- For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
 
 
+
 # Write your MySQL query statement below
-SELECT Salary FROM Employee GROUP BY Salary
-UNION ALL (SELECT NULL AS SecondHighestSalary)
-ORDER BY Salary DESC LIMIT 1 OFFSET 1;
+SELECT MAX(Salary) as SecondHighestSalary FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
