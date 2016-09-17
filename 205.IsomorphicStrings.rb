bcedf60d2fd0572af9254e63934af2812d2ff23d
Hang Cui <oscarcuihang@hotmail.com>
2016-09-16 20:17:48 -0500

    205

---
+++ 205.IsomorphicStrings.rb
@@ -0,0 +1,38 @@
+# Given two strings s and t, determine if they are isomorphic.
+
+# Two strings are isomorphic if the characters in s can be replaced to get t.
+
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+# For example,
+# Given "egg", "add", return true.
+
+# Given "foo", "bar", return false.
+
+# Given "paper", "title", return true.
+
+# Note:
+# You may assume both s and t have the same length.
+
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+    n = s.size
+    return false if n != t.size
+    m1 = {}
+    m2 = {}
+    s.chars.each_with_index do |ch, idx|
+        tc = t[idx]
+    
+        if m1.key?(ch) || m2.key?(tc)
+            return false if m1[ch] != tc
+            return false if m2[tc] != ch
+        else
+            m1[ch] = tc
+            m2[tc] = ch
+        end
+    end
+    true
+end
