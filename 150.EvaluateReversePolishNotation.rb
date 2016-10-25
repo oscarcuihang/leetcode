47aeed3090ec1857283c17f3ed5f2a544b8eafae
Hang Cui <oscarcuihang@hotmail.com>
2016-10-24 23:56:32 -0500

    150

---
+++ 150.EvaluateReversePolishNotation.rb
@@ -0,0 +1,32 @@
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+# Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+# Some examples:
+#   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+#   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
+# @param {String[]} tokens
+# @return {Integer}
+def eval_rpn(tokens)
+    s = []
+    tokens.each do |token|
+        case token
+        when '+'
+            s << (s.pop + s.pop)
+        when '-'
+            v1 = s.pop
+            v2 = s.pop;
+            s << (v2 - v1)
+        when '*'
+            s << (s.pop * s.pop)
+        when '/'
+            v1 = s.pop 
+            v2 = s. pop
+            s << (v2.fdiv(v1).truncate)
+        else
+            s << token.to_i
+        end
+    end
+    s.pop
+end
