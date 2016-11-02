37c7f0a48f3eddd41e94e3d6e4efb7e25ca13ae2
Hang Cui <oscarcuihang@hotmail.com>
2016-11-01 22:52:45 -0500

    129

---
+++ 129.SumRootToLeafNumbers.rb
@@ -0,0 +1,37 @@
+# Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+# An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+# Find the total sum of all root-to-leaf numbers.
+
+# For example,
+
+#     1
+#    / \
+#   2   3
+# The root-to-leaf path 1->2 represents the number 12.
+# The root-to-leaf path 1->3 represents the number 13.
+
+# Return the sum = 12 + 13 = 25.
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer}
+def sum_numbers(root)
+    sumnumbers(root, 0)
+end
+
+def sumnumbers(root, sum)
+    return 0 if root.nil?
+    sum = sum * 10 + root.val
+    return sum if root.right.nil? && root.left.nil?
+    sumnumbers(root.left, sum) + sumnumbers(root.right, sum)
+end
