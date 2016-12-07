0c483d76a416637ab82b405d4b005fdfc2bd7ab9
Hang Cui <oscarcuihang@hotmail.com>
2016-12-07 00:24:34 -0600

    409

--- 290.WordPattern.rb
+++ 290.WordPattern.rb
@@ -15,15 +15,15 @@
 # @param {String} str
 # @return {Boolean}
 def word_pattern(pattern, str)
-    pattern = pattern.chars
-    str = str.split(' ')
-    return false if pattern.size != str.size
-    
-    p2s, s2p = {}, {}
-    pattern.zip(str) do |p, s|
+  pattern = pattern.chars
+  str = str.split(' ')
+  return false if pattern.size != str.size
+  
+  p2s, s2p = {}, {}
+  pattern.zip(str) do |p, s|
     return false if p2s.key?(p) && p2s[p] != s
     return false if s2p.key?(s) && s2p[s] != p
     p2s[p], s2p[s] = s, p
-    end
-    return true
+  end
+  return true
 end
--- 409.LongestPalindrome.rb
+++ 409.LongestPalindrome.rb
@@ -1,1 +1,53 @@
-409. Longest Palindrome.rb
+# 409. Longest Palindrome.rb
+
+# Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+# This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+# Note:
+# Assume the length of given string will not exceed 1,010.
+
+# Example:
+
+# Input:
+# "abccccdd"
+
+# Output:
+# 7
+
+# Explanation:
+# One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+# @param {String} s
+# @return {Integer}
+def longest_palindrome(s)
+    return 0 if s.size == 0
+    hash = Hash.new()
+    res = 0
+    odd = 0
+    s.each_char do |char|
+        if hash.has_key?(char)
+            hash[char] += 1
+        else
+            hash[char] = 1 
+        end
+    end
+    print hash
+    hash.each do |k, v|
+        res += v
+        if v%2==1
+            res -= 1
+            odd += 1
+        end
+    end
+    return res + 1 if odd > 0
+    res
+end
+
+# 统计每个字母的出现次数：
+
+# 若字母出现偶数次，则直接累加至最终结果
+
+# 若字母出现奇数次，则将其值-1之后累加至最终结果
+
+# 若存在出现奇数次的字母，将最终结果+1
